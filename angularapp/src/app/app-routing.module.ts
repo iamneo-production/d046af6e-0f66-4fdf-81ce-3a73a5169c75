8648361d12894669b4125e1d06050ba5f4b21f91
jayanthbharadwaj2601 <kidambi.jayanth@gmail.com>
2022-03-12 13:39:20 +0000

    created dashboard and homepage layout

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
 const routes: Routes = [
   {path:"login", component:LoginComponent},
-  {path:"signup", component:SignupComponent}
+  {path:"signup", component:SignupComponent},
+  {path:"home", component:HomepageComponent}
 ];
 
 @NgModule({
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    DashboardComponent,
+    HomepageComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
