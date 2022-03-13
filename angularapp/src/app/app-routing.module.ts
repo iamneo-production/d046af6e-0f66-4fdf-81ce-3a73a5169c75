ce875559e820ba69daee16ce8d317d76c2818962
jayanthbharadwaj2601 <kidambi.jayanth@gmail.com>
2022-03-13 13:29:51 +0000

    set login as laanding page

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {LoginComponent} from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomepageComponent } from './homepage/homepage.component';
 const routes: Routes = [
-  {path:"login", component:LoginComponent},
+  {path:"", component:LoginComponent},
   {path:"signup", component:SignupComponent},
   {path:"home", component:HomepageComponent}
 ];
