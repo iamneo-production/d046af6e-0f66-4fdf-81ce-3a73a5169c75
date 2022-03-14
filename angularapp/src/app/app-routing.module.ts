69ae3e373a23c7092c209e28182e4e9f7998130c
jayanthbharadwaj2601 <kidambi.jayanth@gmail.com>
2022-03-14 09:43:22 +0000

    made cart page

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
 const routes: Routes = [
   {path:"", component:LoginComponent},
   {path:"signup", component:SignupComponent},
-  {path:"home", component:HomepageComponent}
+  {path:"home", component:HomepageComponent},
+  {path:"cart",component:CartComponent}
 ];
 
 @NgModule({
